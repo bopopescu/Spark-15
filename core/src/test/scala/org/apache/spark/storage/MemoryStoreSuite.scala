416b027a43c0437662172f3ce37e653842489418
Matthew Rennie <matthewrennie@Matthews-MacBook-Pro.local>
2015-03-21 15:16:10 -0700

    started test to ensure access count

---
+++ core/src/test/scala/org/apache/spark/storage/MemoryStoreSuite.scala
@@ -0,0 +1,72 @@
+/*
+ * CMU-SV Research Project
+ */
+
+package org.apache.spark.storage
+
+import scala.language.implicitConversions
+
+import org.scalatest.{FunSuite, BeforeAndAfterEach, Matchers, PrivateMethodTester}
+import org.scalatest.mock._
+import org.mockito.Mockito._
+
+import org.apache.spark.SparkConf
+import org.apache.spark.util._
+
+import java.nio.ByteBuffer
+
+object MemoryStoreSuite {
+
+  final val MAX_MEMORY = 12000  
+
+  case class TestData(blockId:RDDBlockId, initialSize:Int) {
+    lazy val data:ByteBuffer = ByteBuffer.wrap(new Array[Byte](initialSize))
+    def size:Long = initialSize
+  }
+
+  def testData(rddId:Int, splitId:Int, initialSize:Int):TestData = {
+    TestData(RDDBlockId(rddId, splitId), initialSize)
+  }
+}
+
+class MemoryStoreSuite extends FunSuite with Matchers with BeforeAndAfterEach  
+  with PrivateMethodTester with ResetSystemProperties {
+
+  import MemoryStoreSuite._  
+  
+  val tryToPutValue = PrivateMethod[ResultWithDroppedBlocks]('tryToPut)
+  var memoryStore:MemoryStore = _  
+
+  override def beforeEach(): Unit = {
+    val blockManager = mock(classOf[BlockManager])
+    val sparkConf = mock(classOf[SparkConf])
+    when(blockManager.conf).thenReturn(sparkConf)
+    memoryStore = new MemoryStore(blockManager, MAX_MEMORY)
+  }
+
+  override def afterEach(): Unit = {
+    memoryStore.clear()
+    memoryStore = null
+  }
+
+  test("tryToPut should add an item to our data structure") {    
+    val a1 = testData(0, 0, 4000)
+    val a2 = testData(0, 1, 4000)
+    memoryStore invokePrivate tryToPutValue(a1.blockId, a1.data, a1.size, false)
+    memoryStore invokePrivate tryToPutValue(a2.blockId, a2.data, a2.size, false)
+    assert(memoryStore.contains(a1.blockId), "a1 was not in memory store")
+    assert(memoryStore.contains(a2.blockId), "a2 was not in memory store")
+    //TODO: assert that our data structure contains a1 and a2
+    // pending
+  }
+
+  test("each access should increase the access count in our data structure") {
+    val a1 = testData(0, 0, 4000)
+    memoryStore invokePrivate tryToPutValue(a1.blockId, a1.data, a1.size, false)
+    assert(memoryStore.getSize(a1.blockId) == 4000, "a1 had unexpected size")
+    memoryStore.getBytes(a1.blockId)
+    memoryStore.getValues(a1.blockId)
+    //TODO: assert access count increases
+    // pending
+  }
+}
