af21a1b02c7e7878b07c23029d6e1702b55b736a
Matthew Rennie <matthewrennie@Matthews-MacBook-Pro.local>
2015-04-09 07:41:04 -0700

    updated workload generators

--- examples/src/main/scala/edu/cmu/sv/generators/InteractiveWorkloadTest.scala
+++ examples/src/main/scala/edu/cmu/sv/generators/InteractiveWorkloadTest.scala
@@ -4,11 +4,18 @@ import scala.math.random
 import org.apache.spark._
 import scala.util.Random
 
-/** generates an interactive workload */
+/* 
+ * generates an interactive workload
+ * execute with: 
+ *    ./bin/spark-submit --class edu.cmu.sv.generators.InteractiveWorkloadTest --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar
+ *    you can also use --master local[512]
+ */
 object InteractiveWorkloadTest {
 
+  final val ITERATIONS = 1000
   final val CONCURRENT_WORKLOADS = 5
-  final val MAX_WAIT_TIME = 5000
+  final val MIN_WAIT_TIME = 1000
+  final val MAX_WAIT_TIME = 10000
 
   class Workload(_spark:SparkContext) extends Thread with PiApproximation {
 
@@ -16,7 +23,7 @@ object InteractiveWorkloadTest {
     var pi:Double = -1
 
     override def run {
-      Thread.sleep(new Random(MAX_WAIT_TIME).nextInt)
+      Thread.sleep(math.max(new Random(MAX_WAIT_TIME).nextInt, MIN_WAIT_TIME))
       pi = approximatePi
     }
   }
@@ -26,11 +33,13 @@ object InteractiveWorkloadTest {
     val conf = new SparkConf().setAppName("Interactive Workload")
     implicit val spark = new SparkContext(conf)
 
-    val threads = List.fill(CONCURRENT_WORKLOADS)(new Workload(spark))
-    threads.foreach(_.start)
-    threads.foreach(_.join)
-    threads.foreach { wl =>
-      println(s"pi is approximately: ${wl.pi}")
+    for(i <- 1 to ITERATIONS) {
+      val threads = List.fill(CONCURRENT_WORKLOADS)(new Workload(spark))
+      threads.foreach(_.start)
+      threads.foreach(_.join)
+      threads.foreach { wl =>
+        println(s"iteration $i : pi is approximately: ${wl.pi}")
+      }
     }
 
     spark.stop()
--- examples/src/main/scala/edu/cmu/sv/generators/IterativeWorkloadTest.scala
+++ examples/src/main/scala/edu/cmu/sv/generators/IterativeWorkloadTest.scala
@@ -3,18 +3,25 @@ package edu.cmu.sv.generators
 import scala.math.random
 import org.apache.spark._
 
-/** generates an iterative workload */
+/* 
+ * generates an iterative workload
+ * execute with: 
+ *    ./bin/spark-submit --class edu.cmu.sv.generators.IterativeWorkloadTest --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar
+ *    you can also use --master local[512]
+ */
 object IterativeWorkloadTest extends PiApproximation {
 
-  final val ITERATIONS = 5
+  final val ITERATIONS = 5000
 
   def main(args: Array[String]) {
   	
     val conf = new SparkConf().setAppName("Iterative Workload")
     implicit val spark = new SparkContext(conf)
     
-    for(i <- 0 to ITERATIONS-1) {
+    //every second for 5000 iterations
+    for(i <- 1 to ITERATIONS) {
     	println(s"iteration $i - Pi is roughly: " + approximatePi)
+      Thread.sleep(1000)
     }
 
     spark.stop()
