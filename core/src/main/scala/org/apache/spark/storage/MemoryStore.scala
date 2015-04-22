879117148083ed27e56be10c2f38284938d3f308
Chih <huchihc@gmail.com>
2015-04-21 19:56:05 -0700

    Updating the population of useBayes flag
    This somehow didn’t make it in the a few of the previous commits,
    adding it in again.

--- core/src/main/scala/org/apache/spark/storage/MemoryStore.scala
+++ core/src/main/scala/org/apache/spark/storage/MemoryStore.scala
@@ -61,8 +61,7 @@ private[spark] class MemoryStore(blockManager: BlockManager, maxMemory: Long)
   private[spark] val usage = new LinkedHashMap[BlockId, LinkedList[Long]]()
   private[spark] val hitMiss = new LinkedHashMap[BlockId, LinkedList[Boolean]]() //hit is true
 
-  //TODO: get value of useBayes from configuration, false stands for not using bayes.
-  val useBayes = true
+  val useBayes = java.lang.Boolean.valueOf(System.getProperty("CMU_USEBAYES_FLAG","false"))
   var dataset : DataSet = null
   var eva : Evaluation = null
 
