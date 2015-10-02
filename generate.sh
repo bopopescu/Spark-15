0694f43a763794e9ebaa0c382f667d60dcf5550d
dolphinDoReMi <jiajie.liang@west.cmu.edu>
2015-10-01 21:49:13 -0700

    [Error Fix] fix path of spark-examples-1.3.0-SNAPSHOT-hadoop2.3.0.jar, reduce the duplicate running experiments from three times to once

--- generate.sh
+++ generate.sh
@@ -1,11 +1,4 @@
-./bin/spark-submit --class edu.cmu.sv.generators.IterativeWorkloadTest --executor-memory 512g --driver-memory 1g --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar 10 trace 2 0
-./bin/spark-submit --class edu.cmu.sv.generators.InteractiveWorkloadTest --executor-memory 512g --driver-memory 1g --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar 10 trace 2 0
-./bin/spark-submit --class edu.cmu.sv.generators.RandomWorkloadTest --executor-memory 512g --driver-memory 1g --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar 10 trace 2 0
-
-./bin/spark-submit --class edu.cmu.sv.generators.IterativeWorkloadTest --executor-memory 512g --driver-memory 1g --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar 10 trace 2 1
-./bin/spark-submit --class edu.cmu.sv.generators.InteractiveWorkloadTest --executor-memory 512g --driver-memory 1g --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar 10 trace 2 1
-./bin/spark-submit --class edu.cmu.sv.generators.RandomWorkloadTest --executor-memory 512g --driver-memory 1g --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar 10 trace 2 1
-
-./bin/spark-submit --class edu.cmu.sv.generators.IterativeWorkloadTest --executor-memory 512g --driver-memory 1g --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar 10 trace 2 2
-./bin/spark-submit --class edu.cmu.sv.generators.InteractiveWorkloadTest --executor-memory 512g --driver-memory 1g --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar 10 trace 2 2
-./bin/spark-submit --class edu.cmu.sv.generators.RandomWorkloadTest --executor-memory 512g --driver-memory 1g --master local-cluster[2,1,512] ./examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar 10 trace 2 2
+./bin/spark-submit --class edu.cmu.sv.generators.InteractiveWorkloadTest examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop2.3.0.jar 10 trace 2 1
+./bin/spark-submit --class edu.cmu.sv.generators.InteractiveWorkloadTest examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop2.3.0.jar 10 trace 2 2
+./bin/spark-submit --class edu.cmu.sv.generators.InteractiveWorkloadTest examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop2.3.0.jar 10 trace 2 3
+./bin/spark-submit --class edu.cmu.sv.generators.InteractiveWorkloadTest examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop2.3.0.jar 10 trace 2 4
